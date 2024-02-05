c1f086e743222e6cb8b00dbe7efe619c21d3907e
mzt <823682492@qq.com>
2024-02-05 10:10:05 +0800

    update

--- main.go
+++ main.go
@@ -3,22 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
-	_ "github.com/mkevac/debugcharts"
-	"github.com/sirupsen/logrus"
 	"greet_gin/config"
 	"greet_gin/database"
 	"greet_gin/routers"
 	"net/http"
 	_ "net/http/pprof"
+	"runtime"
+	"time"
+
+	_ "github.com/mkevac/debugcharts"
+	"github.com/sirupsen/logrus"
 )
+
+func dummyCPUUsage() {
+	var a uint64
+	var t = time.Now()
+	for {
+		t = time.Now()
+		a += uint64(t.Unix())
+	}
+}
+
+func dummyAllocations() {
+	var d []uint64
+
+	for {
+		for i := 0; i < 2*1024*1024; i++ {
+			d = append(d, 42)
+		}
+		time.Sleep(time.Second * 10)
+		fmt.Println(len(d))
+		d = make([]uint64, 0)
+		runtime.GC()
+		time.Sleep(time.Second * 10)
+	}
+}
 
 func main() {
 
+	go dummyAllocations()
+	go dummyCPUUsage()
 	go func() {
-		err := http.ListenAndServe(":10108", nil)
-		if err != nil {
-			logrus.Error("Error listening")
-		}
+		logrus.Info(http.ListenAndServe(":10108", nil))
 	}()
 	config.Setup()
 
